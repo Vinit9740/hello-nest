769b3d575bc79bc584794a2e9e2ecdd538c57ead
Vinit N K <vinitnk2004@gmail.com>
2026-08-25 16:01:27 +0530

    test with html

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,33 @@
+// import { Controller, Get } from '@nestjs/common';
+// import { AppService } from './app.service';
+
+// @Controller()
+// export class AppController {
+//   constructor(private readonly appService: AppService) { }
+
+//   @Get()
+//   getHello(): string {
+//     return this.appService.getHello();
+//   }
+// }
+
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHome() {
+    return `
+      <html>
+        <body>
+          <h1>Hello from NestJS</h1>
+          <p>This is HTML rendered by NestJS.</p>
+          <button onclick="alert('Clicked!')">
+            Click Me
+          </button>
+        </body>
+      </html>
+    `;
   }
 }
